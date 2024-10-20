294d6c58ebd9c2cd6a15a824ea929aaeaced6f06
Deftu <me@deftu.dev>
2024-10-20 11:07:09 +0200

    Make the command actually work

--- src/main/java/org/polyfrost/colorsaturation/command/SaturationCommand.java
+++ src/main/java/org/polyfrost/colorsaturation/command/SaturationCommand.java
@@ -2,12 +2,14 @@ package org.polyfrost.colorsaturation.command;
 
 import org.polyfrost.colorsaturation.ColorSaturation;
 import org.polyfrost.oneconfig.api.commands.v1.factories.annotated.Command;
+import org.polyfrost.utils.v1.dsl.ScreensKt;
 
 @Command(value = ColorSaturation.MODID, description = "Access the " + ColorSaturation.NAME + " GUI.")
 public class SaturationCommand {
     @Command
-    private void handle() {
+    private void main() {
         // TODO Implement openGui
         // ColorSaturation.config.openGui();
+        ScreensKt.openUI(ColorSaturation.config);
     }
 }
